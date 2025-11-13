26306b8d57348c2cb39369a2b811421c91db6336
BzRTech <contato.bzrtech@gmail.com>
2025-11-13 19:10:31 -0300

    code clean and move files

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Todas as rotas da API começam com /api/
-    # path('students/', include('students.urls')),  # Pode comentar/remover as views antigas
+    path('api/', include('api.urls')),
 ]
